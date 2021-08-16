c2009f499e1eefef1c027972b62809323821fc41
Phillipe Wernette <60796799+pwernette@users.noreply.github.com>
2021-08-16 15:40:58 -0700

    initial revisions

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# relative_relief
+# Relative Relief
+
+The purpose of this program is to
+
+	- import a ENVI format DEM (.dat and .h file); extract beach, dune, and island topological features; and compute landform width and volume.
+
+The program requires the following inputs:
+		1) Input filename (excluding extension)
+		
+		2) window size to calculate statistics
+		
+		3) desired product:
+			rr --> outputs relative relief rasters only
+			
+			shoreline --> outputs shoreline
+			dunetoe --> outputs dune toe
+			dunecrest --> outputs dune crest
+			duneheel --> outputs dune heel
+			backbarrier --> outputs backbarrier
+			
+			landforms --> outputs all geomorphic feature parameters
+			
+			all --> outputs all products
+			
+		4) output data types:
+			ascii --> only outputs ascii file
+			envi --> only outputs ENVI format raster
+			both --> output both ascii and ENVI files
+
+	Example Usage:
+		program sample 11 all both
+					OR
+		program.exe npilidar 11 all both
+		
+NOTE: At North Padre Island, we found an ideal window size to be approximately 25 meters.
+
+NOTE: The default thresholds are as follows:
+	Shoreline:    0.02 masl
+	Dune Toe:     0.2
+	Dune Crest:   0.8
+	Dune Heel:    0.4
+	BB Shoreline: 1.0 masl
+
+NOTE: This program assumes that the grid resolution is relatively small; therefore, a simple Pythagorean theorem distance is appropriate when calculating metrics.
+
+NOTE: This assumption is not valid for distances larger than ~12 miles (~19 km), since the curvature of the Earth will affect the 3D distance.
---
+++ data_structures.hpp
@@ -0,0 +1,176 @@
+#include <math.h>
+#include <fstream>
+#include <iostream>
+#include <windows.h>
+#include <vector>
+
+using namespace std;
+
+///////////////////////////////////////////////////////////////
+// HEADER INFORMATION
+///////////////////////////////////////////////////////////////
+class Header
+{
+public:
+	string description;
+	int ncols; 			// number of columns
+	int nlines; 			// number of lines
+	int npix;  			// number of pixels
+	int bands; 			// number of bands
+	int headeroffset;
+	string filetype;
+	int datatype;
+	string interleave;		// data interleave format (bsq, bip, or bil)
+	string sensortype;
+	string coordsys;		// coordinate system information
+	float xres;			// resolution (x direction)
+	float yres;			// resolution (y direction)
+	double ulx;			// upper-left x coordinate
+	double uly;			// upper-left y coordinate
+	double ymin;
+	double xmax;
+	double zmin;
+	double zmax;
+	string utm_zone_number;
+	string utm_zone_band;
+	string datum;
+	string units;		// measurement units
+	string proj_string;	// projection string
+
+	bool Initialize(string Fname)
+	{
+		Fname.append(".hdr");
+
+		if(!LoadInParameters(Fname))
+		{
+			cout << "ERROR: Cannot find '" << Fname << endl;
+
+			return false;
+		}
+
+		return true;
+	}
+
+	void defaultHeader();
+
+	bool LoadInParameters(string Fname);
+
+	//function to write binary data file
+	void writeDAT(string fn, vector<float> outdat);
+	void writeDAT(string fn, vector<unsigned int> outdat);
+	void writeDAT(string fn, vector<int> outdat);
+	void writeDAT(string fn, vector<long int> outdat);
+	void writeDAT(string fn, vector<float> outdat);
+	void writeDAT(string fn, vector<double> outdat);
+
+	//function to write header file
+	void writeHDR(string fn, vector<float> outinfo);
+	void writeHDR(string fn, vector<unsigned int> outinfo);
+	void writeHDR(string fn, vector<int> outinfo);
+	void writeHDR(string fn, vector<long int> outinfo);
+	void writeHDR(string fn, vector<float> outinfo);
+	void writeHDR(string fn, vector<double> outinfo);
+};
+
+///////////////////////////////////////////////////////////////
+// THRESHOLDS INFORMATION
+///////////////////////////////////////////////////////////////
+class Params
+{
+	public:
+	//-------------------------------------------------------------------
+	//  input parameters
+	//-------------------------------------------------------------------
+
+	//input filename
+	string iFile;
+
+	//initial window size (pixels in data units)
+	int iWindowSize;
+
+	//specify the output product
+	string oProduct;
+
+	//output product format
+	string oFormat;
+
+
+	//-------------------------------------------------------------------
+	//  used to define the thresholds
+	//-------------------------------------------------------------------
+	float tShoreline;
+	float tDT;
+	float tDC;
+	float tDH;
+	float tBB;
+	float tDuneDistMin;
+	float tDuneDistMax;
+	float tCrestDistMin;
+	float tCrestDistMax;
+	float tHeelDistMin;
+	float tHeelDistMax;
+
+	bool Initialize()
+	{
+	if(!LoadInParameters("params_rr.ini"))
+		{
+			cout << "ERROR: Cannot find 'params_rr.ini'" << endl;
+			//MessageBox(NULL, "Cannot find 'params_rr.ini'", "Error", 0);
+
+			return false;
+		}
+
+		if(fmod(Params::iWindowSize, 2) != 1){ // exit the program if the specified window size is not an odd number
+			cout << "ERROR: Invalid window size --> Window size must be an odd number!" << endl;
+			exit(1);
+		} else if(Params::iWindowSize < 3){
+			cout << "ERROR: Invalid window size --> Window size must be at least 3!" << endl;
+			exit(1);
+		}
+
+		return true;
+	}
+	bool LoadInParameters(const char* szFileName);
+};
+
+
+///////////////////////////////////////////////////////////////
+// STORE RASTER VALUES AND METRICS
+///////////////////////////////////////////////////////////////
+class Raster
+{
+public:
+	//integer corresponding to the number of pixels
+	int size;
+
+	//DEM information
+	vector<float> x;			// x coordinate
+	vector<float> y;			// y coordinate
+	vector<float> z;			// z coordinate
+
+	//binary indicating whether the location has data for all data types
+	vector<unsigned int> complete;
+
+	//relative relief variables (per pixel)
+	vector<float> res;
+	vector<float> res_plus1;
+	vector<float> res_plus2;
+	vector<float> avg;
+
+	//binary indicators of feature position
+	vector<float> shoreline;
+	vector<float> dune_toe_line;
+	vector<float> dune_ridge_line;
+	vector<float> dune_heel_line;
+	vector<float> backbarrier_line;
+
+	void Initialize(Params prms, Header hdr);
+
+	void Init(int m_size);
+
+	bool readDAT(string Fname, Header hdr);
+
+	void writeENVIs(string filename, Header hdr);
+
+	void computeRelativeRelief(int i, int j, int buf, Header hdr);
+};
